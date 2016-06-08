1641e5e39e56f18b8ec87612841f355cb164e3db
lepipele <lepipele@users.noreply.github.com>
2016-06-07 22:46:18 -0500

    Initial commit

---
+++ Util.Bitcoin/BitcoinFacade.cs
@@ -0,0 +1,21 @@
+﻿using Casascius.Bitcoin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util.Bitcoin
+{
+    public class BitcoinFacade
+    {
+        public static bool IsValidWalletAddress(string walletAddr)
+        {
+            byte[] hex = BtcAddressUtil.Base58CheckToByteArray(walletAddr);
+            if (hex == null || hex.Length != 21)
+                return false;
+            else
+                return true;
+        }
+    }
+}
